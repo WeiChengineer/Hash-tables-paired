2cb496c57517399b03cb0b789d7d2d867d4d5777
Sean Hatcher <thiseanguy@gmail.com>
2023-09-23 05:56:54 -0500

    completed constructor and insert methods

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,6 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity).fill(null)
     // Initialize your buckets here
     // Your code here
   }
@@ -30,7 +33,24 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const index = this.hashMod(key)
+    let currNode = this.data[index]
+    let kvPair = new KeyValuePair(key, value)
+    this.count++
+
+    if (!currNode) {
+      this.data[index] = kvPair
+      return
+    }
+    while (currNode) {
+      if (currNode.key === key) {
+        currNode.value = value
+        return
+      }
+      currNode = currNode.next
+    }
+    kvPair.next = this.data[index]
+    this.data[index] = kvPair
   }
 
 
@@ -49,5 +69,17 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   }
 }
 
+// let hashTable = new HashTable(2);
+
+// console.log(hashTable.capacity)//.to.equal(2);
+
+// hashTable.insert("key2", "value2");
+// hashTable.insert("key4", "value4");
+
+// console.log("line82",hashTable.data[1].next.key)//.to.equal("key2")
+// console.log(hashTable.data[1].key)//.to.equal("key4")
+
+// console.log(hashTable.data[1].next.value)//.to.equal("value2")
+// console.log(hashTable.data[1].value)//.to.equal("value4")
 
 module.exports = HashTable;
